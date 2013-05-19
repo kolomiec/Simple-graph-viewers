b6cda524ac1b386faaf240edcf0a79cd02274597
kolomiec <kolomeicsergeyk@gmail.com>
2013-05-19 10:44:15 +0000

    Database java bean.Database SQLite helper.Fix bug

--- java/uk/ks/simple/graphviewers/databases/GraphSQLiteHelper.java
+++ java/uk/ks/simple/graphviewers/databases/GraphSQLiteHelper.java
@@ -1,6 +1,9 @@
 package uk.ks.simple.graphviewers.databases;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by root on 5/18/13.
@@ -38,7 +41,7 @@ public class GraphSQLiteHelper extends SQLiteOpenHelper {
 			+ X_COORDINATE + " integer not null, "
 			+ Y_COORDINATE + " integer not null);";
 
-	public MySQLiteHelper(Context context) {
+	public GraphSQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
@@ -51,9 +54,9 @@ public class GraphSQLiteHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(MySQLiteHelper.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
+		Log.w(GraphSQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_GRAPHICS);
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_POINTS_TO_GRAPHIC);
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_POINTS);
