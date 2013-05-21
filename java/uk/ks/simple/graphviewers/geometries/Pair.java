0fcde675cac825384b01cc75274a1bc1ca4d98e8
kolomiec <kolomeicsergeyk@gmail.com>
2013-05-21 16:22:32 +0000

    code clean up

--- java/uk/ks/simple/graphviewers/geometries/Pair.java
+++ java/uk/ks/simple/graphviewers/geometries/Pair.java
@@ -4,35 +4,30 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
+import java.util.Comparator;
+
 import uk.ks.simple.graphviewers.beans.Point;
 
 /**
  * Created by root on 5/17/13.
  */
-public class Pair {
+public class Pair implements Comparable<Pair> {
 
 	private int color = Color.BLACK;
-	private final Point point;
+	private Point point = new Point(0, 0);
 	private Paint paint = new Paint();
-	private final int diameter = 5;
+	private final int diameter = 3;
 
-	{
-		paint.setStrokeWidth(diameter);
-		paint.setColor(this.color);
-	}
-
-	public Pair(Point point) {
-		this.point = point;
-	}
 
 	public Pair(int color, Point point) {
 		this.color = color;
 		this.point = point;
+        paint.setColor(color);
 	}
 
 	public void draw(Canvas canvas) {
         Point point = recalculateCoordinate();
-		canvas.drawPoint(point.getX(), point.getY(), paint);
+        canvas.drawCircle(point.getX(), point.getY(), diameter, paint);
 	}
 
     private Point recalculateCoordinate() {
@@ -55,4 +50,27 @@ public class Pair {
 	public void setColor(int color) {
 		this.color = color;
 	}
+
+    @Override
+    public int compareTo(Pair pair) {
+        return this.point.getX() - pair.getPoint().getX();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Pair that = (Pair) obj;
+
+        if (this.getPoint().getX() != that.getPoint().getX()) return false;
+        if (this.getPoint().getY() != that.getPoint().getY()) return false;
+        if (this.color != that.color) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair[x=" + point.getX() + ", y=" + point.getY() + ", color=" + this.color + "]";
+    }
 }
